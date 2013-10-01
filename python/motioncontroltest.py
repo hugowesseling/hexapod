24ef03b6219237ccbf95d4ced5ad3d69fb124341
Hugo Wesseling <hugowesseling@gmail.com>
2013-10-02 00:08:22 +0200

    Update motioncontroltest to send test walking commands.

--- python/motioncontroltest.py
+++ python/motioncontroltest.py
@@ -8,15 +8,17 @@ counter=0
 while True:
     counter+=1
     print "Sending"
-    rotx=math.sin(counter*0.08)*0.1
-    roty=math.sin(counter*0.13)*0.1
-    rotz=math.sin(counter*0.20)*0.1
-    sock.send("R %f %f %f"%(rotx,roty,rotz))
-    while True:
+    #rotx=math.sin(counter*0.08)*0.1
+    #roty=math.sin(counter*0.13)*0.1
+    #rotz=math.sin(counter*0.20)*0.1
+    #sock.send("R %f %f %f"%(rotx,roty,rotz))
+    #sock.send(".")
+    sock.send("W 0.0 1.0 0.01 20")
+    for i in range(20):
       print "Testing to receive"
       received,buf=sock.receive()
       if received:
         print "Received:",buf
         break
-      time.sleep(0.01)
+      time.sleep(0.1)
 
