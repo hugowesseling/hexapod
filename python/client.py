178265a5b9c9190e624809d67509cbef011a9080
unknown <hugowesseling@gmail.com>
2015-03-18 22:22:34 +0100

    Updated client to connect to tau ip

--- python/client.py
+++ python/client.py
@@ -4,7 +4,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
-host = '10.0.25.104'
+host = '192.168.1.84'
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
